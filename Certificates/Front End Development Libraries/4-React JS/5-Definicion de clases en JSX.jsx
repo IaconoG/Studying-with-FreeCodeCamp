e0242c88577a87ad086e31c0d4085d1065fd2e5e
iacono gianfranco <iacono.gian13@gmail.com>
2023-02-04 22:47:37 -0300

    Clases en JSX

---
+++ Certificates/Front End Development Libraries/4-React JS/5-Definicion de clases en JSX.jsx
@@ -0,0 +1,6 @@
+const JSX = (
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
+  </div>
+);
+// La convención de nomenclatura para todos los atributos HTML y referencias a eventos en JSX se convierte a camelCase.
